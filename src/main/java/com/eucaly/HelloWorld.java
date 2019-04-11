21fa7af78278b1a3ef1bf487d3f1a8472750dc40
eucal <eucaly1026@gmail.com>
2019-04-11 19:52:51 +0800

    Java and Kotlin variable, data type.

--- src/main/java/com/eucaly/HelloWorld.java
+++ src/main/java/com/eucaly/HelloWorld.java
@@ -5,5 +5,15 @@ public class HelloWorld {
 //        System.out.println("Hello World");
         Person p = new Person();
         p.hello();
+        int age = 19;
+        Integer age2 = 19;
+        char c = '我';
+        Character c2 = 'A';
+        byte b = 120;
+        float weight = 44.8f;
+        float height = 1.56f;
+        boolean adult = true;
+        boolean enroll = false;
+        String name = "Eucaly";
     }
 }
